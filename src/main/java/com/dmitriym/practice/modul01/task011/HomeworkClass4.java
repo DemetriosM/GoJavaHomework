e19eb7b379632db0cd9ebbdc67ec03631f8d1010
DemetriosM <dmdonetsk@outlook.com>
2017-01-31 10:56:01 +0200

    Add first homework

---
+++ src/main/java/com/dmitriym/practice/modul01/task011/HomeworkClass4.java
@@ -0,0 +1,10 @@
+package com.dmitriym.practice.modul01.task011;
+
+/**
+ * Created by ДМ on 31.01.2017.
+ */
+public class HomeworkClass4 {
+    public static void main(String[] args) {
+        System.out.printf("Мой четвертый класс на Java");
+    }
+}
