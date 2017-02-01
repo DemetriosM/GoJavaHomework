12a488a982b57e241528eecd03a8362d28476ece
DemetriosM <dmdonetsk@outlook.com>
2017-02-01 08:40:31 +0200

    renamed classes, etc

---
+++ src/main/java/ua/goit/module/first/homework/SecondClass.java
@@ -0,0 +1,7 @@
+package ua.goit.module.first.homework;
+
+public class SecondClass {
+    public static void main(String[] args) {
+        System.out.printf("Мой первый класс на Java");
+    }
+}
--- src/main/java/ua/goit/module/first/homework/ThirdClass.java
+++ src/main/java/ua/goit/module/first/homework/ThirdClass.java
@@ -1,9 +1,6 @@
-package com.dmitriym.practice.modul01.task011;
+package ua.goit.module.first.homework;
 
-/**
- * Created by ДМ on 31.01.2017.
- */
-public class FirstClass {
+public class ThirdClass {
     public static void main(String[] args) {
         System.out.printf("Мой первый класс на Java");
     }
