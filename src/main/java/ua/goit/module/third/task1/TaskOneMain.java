97c27a74c3886279c1f3f11396d369d800a83b91
DemetriosM <dmdonetsk@outlook.com>
2017-02-16 08:29:03 +0200

    fixed task1 in module 3 and task1 in module 4

--- src/main/java/ua/goit/module/third/task1/TaskOneMain.java
+++ src/main/java/ua/goit/module/third/task1/TaskOneMain.java
@@ -1,10 +1,9 @@
 package ua.goit.module.third.task1;
 
 public class TaskOneMain {
+    public static final String[] WORDS_OF_SONGS = {"walking", "flying", "singing", "Bird"};
     public static void main(String[] args) {
         Bird bird = new Bird();
-        final String[] WORDSOFSONGS = {"walking", "flying", "singing", "Bird"};
-        bird.sing(WORDSOFSONGS[3]);
-        bird.sing(WORDSOFSONGS);
+        bird.sing(WORDS_OF_SONGS);
     }
 }
