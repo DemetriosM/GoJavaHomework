03b035f4a9959bb8e585b64aa84f0eff5654de0d
DemetriosM <dmdonetsk@outlook.com>
2017-03-20 12:43:39 +0200

    added module 7 task 4

--- src/main/java/ua/goit/module/seventh/task4/Main.java
+++ src/main/java/ua/goit/module/seventh/task4/Main.java
@@ -1,7 +1,5 @@
 package ua.goit.module.seventh.task4;
 
-import java.time.Instant;
-import java.time.temporal.ChronoField;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
@@ -12,29 +10,99 @@ public class Main {
         List<String> stringArrayList = new ArrayList();
         List<Integer> intLinkedList = new LinkedList<>();
         List<String> stringLinkedList = new LinkedList<>();
+
+        System.out.println("ArrayList<Integer> with 1000 elements");
         generateIntegerList(intArrayList, 1000);
-        measureAddTime(intArrayList, 1);
+        measureTimeAdding(intArrayList, 1, "Add");
+        measureTimeDeleting(intArrayList, 1, "Remove");
+        measureTimeSet(intArrayList, 0, 1, "Set");
+        measureTimeGet(intArrayList, 0, "Get");
+        System.out.println("\nLinkedList<Integer> with 1000 elements");
+        generateIntegerList(intLinkedList, 1000);
+        measureTimeAdding(intLinkedList, 1, "Add");
+        measureTimeDeleting(intLinkedList, 1, "Remove");
+        measureTimeSet(intLinkedList, 0, 1, "Set");
+        measureTimeGet(intLinkedList, 0, "Get");
 
+        System.out.println("\nArrayList<Integer> with 10000 elements");
+        generateIntegerList(intArrayList, 10000);
+        measureTimeAdding(intArrayList, 1, "Add.");
+        measureTimeDeleting(intArrayList, 1, "Remove");
+        measureTimeSet(intArrayList, 0, 1, "Set");
+        measureTimeGet(intArrayList, 0, "Get");
+        System.out.println("\nLinkedList<Integer> with 10000 elements");
+        generateIntegerList(intLinkedList, 10000);
+        measureTimeAdding(intLinkedList, 1, "Add");
+        measureTimeDeleting(intLinkedList, 1, "Remove");
+        measureTimeSet(intLinkedList, 0, 1, "Set");
+        measureTimeGet(intLinkedList, 0, "Get");
 
+        System.out.println("\nArrayList<String> with 1000 elements");
+        generateStringList(stringArrayList, 1000);
+        measureTimeAdding(stringArrayList,"string", "Add");
+        measureTimeDeleting(stringArrayList, "string", "Remove");
+        measureTimeSet(stringArrayList, 0, "string", "Set");
+        measureTimeGet(stringArrayList, 0, "Get");
+        System.out.println("\nLinkedList<String> with 1000 elements");
+        generateStringList(stringLinkedList, 1000);
+        measureTimeAdding(stringLinkedList,"string", "Add");
+        measureTimeDeleting(stringLinkedList, "string", "Remove");
+        measureTimeSet(stringLinkedList, 0, "string", "Set");
+        measureTimeGet(stringLinkedList, 0, "Get");
+
+        System.out.println("\nArrayList<String> with 10000 elements");
+        generateStringList(stringArrayList,10000);
+        measureTimeAdding(stringArrayList,"string", "Add");
+        measureTimeDeleting(stringArrayList, "string", "Remove");
+        measureTimeSet(stringArrayList, 0, "string", "Set");
+        measureTimeGet(stringArrayList, 0, "Get");
+        System.out.println("\nLinkedList<String> with 10000 elements");
+        generateStringList(stringLinkedList, 10000);
+        measureTimeAdding(stringLinkedList,"string", "Add");
+        measureTimeDeleting(stringLinkedList, "string", "Remove");
+        measureTimeSet(stringLinkedList, 0, "string", "Set");
+        measureTimeGet(stringLinkedList, 0, "Get");
     }
 
     public static void generateIntegerList(List<Integer> list, int size){
+        list.clear();
         for (int i = 0; i < size; i++){
             list.add(i);
         }
     }
 
     public static void generateStringList(List<String> list, int size){
+        list.clear();
         for (int i = 0; i < size; i++){
             list.add("string " + i);
         }
     }
 
-    public static <T> void measureAddTime(List<T> list, T item){
-        Instant start = Instant.now();
+    public static <T> void measureTimeAdding(List<T> list, T item, String method){
+        long duration, startTime = System.nanoTime();
         list.add(item);
-        Instant end = Instant.now();
-        System.out.printf("Item was added in %d nanoseconds to %s",
-                end.getLong(ChronoField.NANO_OF_SECOND) - start.getLong(ChronoField.NANO_OF_SECOND), list.getClass());
+        duration = System.nanoTime() - startTime;
+        System.out.println(String.format("%s. Execution time: %d ns", method, duration));
+    }
+
+    public static <T> void measureTimeDeleting(List<T> list, T item, String method){
+        long duration, startTime = System.nanoTime();
+        list.remove(item);
+        duration = System.nanoTime() - startTime;
+        System.out.println(String.format("%s. Execution time: %d ns", method, duration));
+    }
+
+    public static <T> void measureTimeSet(List<T> list, int index, T item, String method){
+        long duration, startTime = System.nanoTime();
+        if (index < list.size() && index >= 0) list.set(index, item);
+        duration = System.nanoTime() - startTime;
+        System.out.println(String.format("%s. Execution time: %d ns", method, duration));
+    }
+
+    public static <T> void measureTimeGet(List<T> list, int index, String method){
+        long duration, startTime = System.nanoTime();
+        if (index < list.size() && index >= 0) list.get(index);
+        duration = System.nanoTime() - startTime;
+        System.out.println(String.format("%s. Execution time: %d ns", method, duration));
     }
 }
